86fb98bdee96ff3cdc31038a7641126d1215d6cb
shotakusakarimt <shota.kusakari@metateam.co.jp>
2024-05-02 10:34:54 +0900

    意外とできた

---
+++ hairetu/hairetu renshuu3/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) { 
+    ArrayList<String> members=new ArrayList<String>();
+    members.add("草苅勝太");
+    members.add("吉田蓮");
+    members.add("清沢海斗");
+    members.add("hase-gawa-sea-Na");
+    members.add("青木朱嶺");
+    
+    for(String i:members){
+        System.out.println(i);
+    }
+  }
+}
